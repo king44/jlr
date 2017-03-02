349aea01170d7730b40744da159aad4f6546b9d1
BJB-00450\Administrator <jlra@qq.com>
2017-03-02 15:36:06 +0800

    no message

--- fist/server.js
+++ fist/server.js
@@ -75,4 +75,14 @@ app.listen(PORT, function () {
 
 
 
+var WebSocketServer = require('ws').Server,
+    wss = new WebSocketServer({ port: 8181 });
+wss.on('connection', function (ws) {
+    console.log('client connected');
+    ws.on('message', function (message) {
+        console.log(message);
+    });
+});
+
+
 
