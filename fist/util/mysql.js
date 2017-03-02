c41f3d67b832aaf06679ba745d125291d73a0aef
BJB-00450\Administrator <jlra@qq.com>
2017-03-02 08:55:46 +0800

    insert改变参数读取方式

--- fist/util/mysql.js
+++ fist/util/mysql.js
@@ -19,30 +19,40 @@ sql_client.connect();
 sql_client.query("use " + TEST_DATABASE);
 
 /*sql_client.query(
-    'SELECT * FROM '+TEST_TABLE,
-    function selectCb(err, results, fields) {
-        if (err) {
-            throw err;
-        }
+ 'SELECT * FROM '+TEST_TABLE,
+ function selectCb(err, results, fields) {
+ if (err) {
+ throw err;
+ }
 
-        if(results)
-        {
-            for(var i = 0; i < results.length; i++)
-            {
-                console.log("%d\t%s\t%s", results[i].id, results[i].name, results[i].age);
-            }
+ if(results)
+ {
+ for(var i = 0; i < results.length; i++)
+ {
+ console.log("%d\t%s\t%s", results[i].id, results[i].name, results[i].age);
+ }
+ }
+ sql_client.end();
+ }
+ );*/
+
+sql_client.insertSql = function(args){
+    var val = "";
+    for (var i= 0 ;i<arguments.length;i++){
+        val += "\'";
+        val+=arguments[i]
+        val +="\'";
+        if(i!=arguments.length-1){
+            val +=',';
         }
-        sql_client.end();
     }
-);*/
 
- sql_client.insertSql = function(u, p, c, l){
-    var val = "\'"+u+"\',\'"+p+"\',\'"+c+"\',\'"+l+"\'";
+    // var val = "\'"+u+"\',\'"+p+"\',\'"+c+"\',\'"+l+"\'";
     var sql_commond = "INSERT INTO user (user, psw, create_date, last_login_date) VALUES ("+val+")";
     console.log('-sql_commond--->>',sql_commond)
     sql_client.query(
         //"INSERT INTO 'user' ('user', 'psw', 'create_date', 'last_login_date') VALUES ("+val+")"
-       // "INSERT INTO 'user' values('user','ww3','2017-11-11','2017-11-12');"
+        // "INSERT INTO 'user' values('user','ww3','2017-11-11','2017-11-12');"
         sql_commond
 
     );
